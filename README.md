247031991826f6f62cc9c7db5901e8652d66cdbf
keatongallagher <keatongallagher@its-MacBook-Pro.local>
2020-02-25 13:28:56 -0700

    added the server and connections to the DB

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # axios-test-data-server
 This is a Go server that connects to ElephantSQL DB and returns the data you are testing for an app. 
+
+## Resources
+
+  [Heroku-config-package](https://github.com/kardianos/govendor)
+
+  [HTTP-ROUTER](https://github.com/gorilla/mux) - install: "go get -u github.com/gorilla/mux"
+
+  [pq](https://github.com/lib/pq) - install: "go get github.com/lib/pq"
+
+  [postman](https://www.getpostman.com/) - for testing endpoints
+
+  [golang-install](https://golang.org/doc/install)
+
+  [postgresql](https://www.postgresql.org/)
+
+  [online-postgresql](https://www.elephantsql.com/)
+
+### postgresql queries
+
+  1. Creating test_items_weboost_cart table
+  
+      create TABLE test_items_weboost_cart (
+      product_id int NOT NULL UNIQUE,  
+      sub_total VARCHAR(5000),
+      item_name VARCHAR(500) NOT NULL,
+      item_handle VARCHAR(500) NOT NULL UNIQUE,
+      item_sku VARCHAR(500) NOT NULL UNIQUE,
+      item_price VARCHAR(500),
+      item_image VARCHAR(500),
+      PRIMARY KEY (product_id)
+    );
+
+  2. Creating a test_items_weboost_cart record
+      INSERT INTO test_items_weboost_cart (
+        product_id, 
+        sub_total, 
+        item_name,
+        item_handle,
+        item_sku,
+        item_price,
+        item_image
+        ) VALUES (
+          value1, 
+          value2, 
+          value3,
+          value4,
+          value5,
+          value6,
+          value7
+        );
+  
+  3. Selecting errors
+
+      select * from testdatacartweboost;
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+)
+
+type singleItem struct {
+	ID     int    `json-:"id"`
+	Total  string `json-:"sub_total"`
+	Name   string `json-:"item_name"`
+	Handle string `json-:"item_handle"`
+	SKU    string `json-:"item_sku"`
+	Price  string `json-:"item_price"`
+	Image  string `json-:"item_image"`
+}
+type serverError struct {
+	Message string
+	Status  int
+}
+
+var db *sql.DB
+
+func main() {
+	pgURL, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err = sql.Open("postgres", pgURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+
+	r := mux.NewRouter()
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					switch t := err.(type) {
+					case string:
+						http.Error(w, t, http.StatusInternalServerError)
+					case error:
+						http.Error(w, t.Error(), http.StatusInternalServerError)
+					case serverError:
+						http.Error(w, t.Message, t.Status)
+					default:
+						http.Error(w, "unknown error", http.StatusInternalServerError)
+					}
+				}
+			}()
+			next.ServeHTTP(w, r)
+		})
+	})
+
+	r.HandleFunc("/weboostCartData", getTestCartData).Methods("GET")
+
+	// Telling the server what it accepts
+	methods := handlers.AllowedMethods([]string{"GET", "PUT", "POST", "DELETE"})
+	headers := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+	origins := handlers.AllowedOrigins([]string{
+		"http://localhost:3000"})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("PORT not set")
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(methods, origins, headers)(r)))
+}
+
+func respondWithError(w http.ResponseWriter, status int, error serverError) {
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(error)
+}
+
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(data)
+}
+
+func getTestCartData(w http.ResponseWriter, r *http.Request) {
+	var items []singleItem
+	var error serverError
+
+	rows, err := db.Query("SELECT * FROM test_items_weboost_cart")
+	if err != nil {
+		error.Message = fmt.Sprintf(`Server Error %s`, err)
+		respondWithError(w, http.StatusInternalServerError, error)
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var total string
+		var name string
+		var handle string
+		var sku string
+		var price string
+		var image string
+
+		err = rows.Scan(&id, &total, &name, &handle, &sku, &price, &image)
+		if err != nil {
+			error.Message = fmt.Sprintf(`Scan Error %s`, err)
+			respondWithError(w, http.StatusInternalServerError, error)
+			return
+		}
+
+		a := singleItem{ID: id, Total: total, Name: name, Handle: handle, SKU: sku, Price: price, Image: image}
+		items = append(items, a)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		error.Message = fmt.Sprintf(`Rows Error %s`, err)
+		respondWithError(w, http.StatusInternalServerError, error)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	responseJSON(w, items)
+}
